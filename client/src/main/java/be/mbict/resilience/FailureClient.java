a85aefa64c95b384240a7dfd0c61a9c47c6fa7fb
Marc Bogaerts <marc.je.bogaerts@telenet.be>
2022-03-05 16:48:36 +0100

    Retry and curcuit breaker on failure endpoints

--- client/src/main/java/be/mbict/resilience/FailureClient.java
+++ client/src/main/java/be/mbict/resilience/FailureClient.java
@@ -1,14 +1,22 @@
 package be.mbict.resilience;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @FeignClient(name = "fail", url = "${client.instances.fail.url}")
 public interface FailureClient {
 
+    @CircuitBreaker(name = "always", fallbackMethod = "fallback")
     @GetMapping("/always")
     String always();
 
+    @Retry(name = "twiice", fallbackMethod = "fallback")
     @GetMapping("/twice")
     String twice();
+
+    default String fallback(Exception e) {
+        return "Fail fallback";
+    }
 }
